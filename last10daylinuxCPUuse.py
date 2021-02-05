ff2d1c8453f52a3725eed5b691875c9802677309
Pravin <pravinv14@gmail.com>
2021-02-05 22:29:34 +0530

    Create last10daylinuxCPUuse.py

---
+++ last10daylinuxCPUuse.py
@@ -0,0 +1,26 @@
+"""
+This script requires sysstat package to be installed to get the historical data
+yum install sysstat -- RHEL/centOS based system
+Also this script get average of the day's CPU usage result
+
+More edits to come.
+
+"""
+
+import os
+from datetime import datetime, timedelta
+
+data = []
+for i in range(10):
+    day = (datetime.now() - timedelta(i)).strftime('%d')
+    cpudata = os.popen('sar -u -f /var/log/sa/sa'+str(day)+' | tail -1').read()[:-1].split(" ")
+    if cpudata[-1] == '':
+        xx = 0
+    else:
+        xx = 100 - float(cpudata[-1])
+    data.append(xx)
+#    print(data)
+
+for i in range(0,10):
+    day = (datetime.now() - timedelta(i)).strftime('%d')
+    print('CPU Usage for {} : '+ str(data[0])).format(day)
