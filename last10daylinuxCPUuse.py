67bbf782670ff76637d4fa2d863e6343d1d90046
Pravin <pravinv14@gmail.com>
2021-02-09 13:08:37 +0530

    Update last10daylinuxCPUuse.py

--- last10daylinuxCPUuse.py
+++ last10daylinuxCPUuse.py
@@ -13,7 +13,7 @@ for i in range(10):
     day = (datetime.now() - timedelta(i)).strftime('%d')
     cpudata = os.popen('sar -u -f /var/log/sa/sa'+str(day)+' | tail -1').read()[:-1].split(" ")
     if cpudata[-1] == '':
-        empty = 0
+        val = 0
     else:
         val = 100 - float(cpudata[-1])
     data.append(val)
